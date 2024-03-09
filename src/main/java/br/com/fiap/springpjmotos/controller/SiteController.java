0a3ddca26a20eeaab0b4918d83ab2c2166f9b1d3
Benefrancis <benefrancis@gmail.com>
2024-03-09 20:32:00 -0300

    thymeleaf

--- src/main/java/br/com/fiap/springpjmotos/controller/SiteController.java
+++ src/main/java/br/com/fiap/springpjmotos/controller/SiteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/site")
+@RequestMapping(value = {"/", "/site"})
 public class SiteController {
     private static final Logger log = LoggerFactory.getLogger( SiteController.class );
 
